b0b02779a78049a2ebb1c625588cf5f8c42e312b
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-10 17:47:48 +0530

    Create print-list.py

---
+++ print-list.py
@@ -0,0 +1,22 @@
+#In list we can use multiple datatype
+
+1. Algorithm:
+list1 = [1, 'hello', 3.14, True]
+print(list1)
+
+Output:
+[1, 'hello', 3.14, True]
+
+#by using "for loop" we can print element 1 by 1.
+2. Algorithm:
+my_list = [1, 'hello', 3.14, True]
+for item in my_list:
+    print(item)
+
+Output: 
+1
+hello
+3.14
+True
+
+
