2727ffc115a3d6a1667420aee2a37562fd2562cb
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-10 17:38:38 +0530

    Update store-value-in-variable.py

--- store-value-in-variable.py
+++ store-value-in-variable.py
@@ -21,3 +21,19 @@ print(b)
 
 Output: 
 hatimloha
+
+4. Algorithm:
+a=5
+b="hatimloha"
+print(a+b)
+
+Output: 
+error (this error come because 2 different type of datatype cant add)
+
+5. Algorithm:
+a=5
+b=10
+print(a+b)
+
+Output: 
+15
