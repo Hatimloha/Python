68231971feaa9fad09b1a0c4381ae12a41f9b3f0
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-11 15:50:18 +0530

    Create type-casting.py

---
+++ type-casting.py
@@ -0,0 +1,20 @@
+#Type casting refers to converting a value from one data type to another. It's like changing the form of information to fit a different context.
+
+1. Algorithm 
+a="1"
+b="5"
+print(int(a) + int(b))
+
+output: 
+6
+
+
+2. Algorithm
+string = "10"
+number = 15 
+string_number = int(string)
+sum = number + int(string) 
+print("the sum of both number is:", sum)
+
+Output: 
+the sum of both number is: 25
