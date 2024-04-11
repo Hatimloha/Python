551d4b4a18e5fdf1fa72fa6399a575f56c4900c4
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-11 17:10:05 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,46 @@
+#How to get input from the user
+
+1. Algorithm: Add
+a = input("enter the first number :")
+b = input("enter the second number :")
+
+print(int(a) + int(b))
+
+Output:
+enter the first number :6 
+enter the second number :5
+11
+
+2. Algorithm: Sub
+a = input("enter the first number :")
+b = input("enter the second number :")
+
+print(int(a) - int(b))
+
+Output: 
+enter the first number :10
+enter the second number :5
+5
+
+3. Algorithm: Multiplication
+a = input("enter the first number :")
+b = input("enter the second number :")
+
+print(int(a) * int(b))
+
+Output: 
+enter the first number :10
+enter the second number :5
+50
+
+
+4. Algorithm: Divide
+a = input("enter the first number :")
+b = input("enter the second number :")
+
+print(int(a) / int(b))
+
+Output: 
+enter the first number :50
+enter the second number :5
+10.0
