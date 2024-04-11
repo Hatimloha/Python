db49090138e19ef9109f7619390eac7dce7f541b
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-11 17:28:48 +0530

    Create string-in-multi-line.py

---
+++ string-in-multi-line.py
@@ -0,0 +1,21 @@
+# Check the question (5 lines) where I had used multiple lines in different lines using triple quotes (''')
+
+1. Algorithm
+name = input("enter your name: ")
+question = '''How are you
+welcome to my GITHUB 
+tell me about you'''
+
+print("hello, " + name)
+print (question)
+
+age = input("write your age show we can suggest containt: ")
+print=("age updated in data" + age)
+
+Output:  
+enter your name: hatim 
+hello, hatim 
+How are you
+welcome to my GITHUB
+tell me about you
+write your age show we can suggest containt: 21
