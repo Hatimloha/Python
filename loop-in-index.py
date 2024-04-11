c8ed34fb860f23fd8f02090306d4b50eea161fa0
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-11 17:48:57 +0530

    Create loop-in-index.py

---
+++ loop-in-index.py
@@ -0,0 +1,17 @@
+# We will use "for" loop to print an index
+
+1. Algorithm 
+name = "github"
+print("lets use a for loop\n")
+for character in name:
+    print(character)
+
+Output: 
+lets use a for loop
+
+g
+i
+t
+h
+u
+b
