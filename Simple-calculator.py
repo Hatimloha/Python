41eb7357706aaabec45b89a8689a9fb0a747d167
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-11 15:25:44 +0530

    Create Simple-calculator.py

---
+++ Simple-calculator.py
@@ -0,0 +1,16 @@
+#Calculator 
+#Oprators: +,-,*,/,//,%
+
+1. Algorithm
+a=10
+b=15
+print("the value of", a,"+", b, "is:", (a + b))
+print("the value of", a,"-", b, "is:", (a - b))
+print("the value of", a,"*", b, "is:", (a * b))
+print("the value of", a,"/", b, "is:", (a / b))
+
+Output:
+the value of 10 + 15 is: 25
+the value of 10 - 15 is: -5
+the value of 10 * 15 is: 150
+the value of 10 / 15 is: 0.6666666666666666
