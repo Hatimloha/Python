88efb9a8731c2100190bceb9a4b61344c19e6c7f
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-04-11 17:35:06 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,22 @@
+# An index is a data structure that allows for efficient lookup of elements based on a key or a position. In programming, an index often refers to
+# In python indexing start from "0" and goes as per string 
+1. Algorithm
+name = "hatim"
+print(name[0])
+
+Output: 
+h
+
+2. Algorithm
+name = "hatim"
+print(name[1])
+
+Output: 
+a
+
+3. Algorithm
+name = "hatim"
+print(name[4])
+
+Output: 
+m
